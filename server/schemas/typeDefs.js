f9a49fef6054f52013edbf6c3f89f7e83774e32d
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-12-02 10:56:36 -0800

    IDefining models in typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,63 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Query {
-        me: String,
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        image: String!
     }
+
+    type Tutorial {
+        _id: ID!
+        title: String!
+        author: String!
+        tags: [Tag]
+        category: String!
+        steps: [Steps]!
+        ratings: [Rating]!
+        searchable: Boolean    
+    }
+
+    type Category {
+        _id: ID!
+        name: String!
+    }
+
+    type Tag {
+        _id: ID!
+        name: String!
+    }
+
+    type Room {
+        _id: ID!
+        owner: 
+        tutorial: 
+        currentStep: 
+    }
+
+    type Step {
+        content: String!
+        comments: [Comment]
+    }
+
+    type Rating {
+        _id: ID!
+        user: [ID]
+        value: Int
+    }
+
+    type Comment {
+        _id: ID!
+        author: 
+        content: String!;
+    }
+
+   
+
+
+
 
 `
 module.exports = typeDefs;
