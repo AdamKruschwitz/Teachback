e631468d8efe5d0d5643b181a9ba6eeb6160ddb0
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-12-02 11:53:06 -0800

    added preview and input states

--- client/src/components/TutorialInput/index.js
+++ client/src/components/TutorialInput/index.js
@@ -1,31 +1,52 @@
 import React, { useState }from "react";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import styled from "styled-components";
+import ReactMarkdown from 'react-markdown';
+import remarkGFM from 'remark-gfm';
 
 
 
 function TutorialInput({ className }) {
     const [body, setBody] = useState("");
+    const [preview, setPreview] = useState(false);
+    // const [markdown, setMarkdown] = useState("");
     const handleChange = (event) => {
         const { name, value } = event.target;
 
         if(name === "Input") {
             setBody(value);
             console.log(body);
+            // setMarkdown(generateMarkdown(value))
         }
     }
+    const togglePreview = (event) => {
+        setPreview(!preview);
+    }
+
+    if(preview)
     return (
-        <TextField 
-            name="Input"
-            className={ className } 
-            placeholder="Write your tutorial here..."
-            multiline 
-            onChange={handleChange}
-        />
+        <div>
+            <Button onClick={togglePreview} children="Preview" />
+            <ReactMarkdown remarkPlugins={[remarkGFM]}>{body}</ReactMarkdown>
+        </div>
     );
+
+    return (
+        <div>
+        <Button onClick={togglePreview} children="Preview" />
+        <TextField 
+            multiline
+            name="Input"
+            value={body}
+            rows={10}
+            onChange={handleChange}
+            className={className}
+            />
+        </div>
+    )
 }
 const StyledTutorialInput = styled(TutorialInput)`
-    height: 100vh !important;
     width: 100vw;
 `
 
