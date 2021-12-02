83fee10383b08be1a166a312b0805347b7f7f15c
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-02 12:11:34 -0800

    fix versions of dependencies in package.json so that firebase works

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import { auth } from './firebase'
+import Login from './components/Login'
+import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
 function App() {
   return (
+    
     <div className="App">
+      <Router>
+        {/* <Login /> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,8 +30,9 @@ function App() {
           Learn React
         </a>
       </header>
+      </Router>
     </div>
   );
-}
+  }
 
 export default App;
