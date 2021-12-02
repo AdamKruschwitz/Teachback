b8a896f7616d2e9f729a2e6ee0867bd00d9de36f
George Sandoval <gssandoval09@gmail.com>
2021-12-01 23:09:06 -0600

    added User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,28 @@
 const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        required: true,
+
+    },
+
+    email: {
+        type: String,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    image: {
+        type: String
+    }
+
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,12 @@ const Category = require('./Category');
 const Tutorial = require('./Tutorial');
 const Rating = require('./Rating');
 const Comment = require('./Comment');
+const User = require('./User')
 
 module.exports = {
     Category,
     Tutorial,
     Rating,
-    Comment
+    Comment,
+    User
 }
