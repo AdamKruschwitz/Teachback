f74fce7efa994157662e5439ae09b5c1a79b317c
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-12-01 20:43:29 -0800

    added tutorial schema

--- server/models/Tutorial.js
+++ server/models/Tutorial.js
@@ -1,1 +1,35 @@
 const { Schema, model } = require('mongoose');
+
+const tutorialSchema = new Schema( {
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    tags: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Tag'
+    }],
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    steps: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Steps'
+    }],
+    ratings: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Rating'
+    }],
+    searchable: {
+        type: Boolean
+    },
+    title: {
+        type: String,
+        trim: true
+    }
+});
+
+const Tutorial = model('Tutorial', tutorialSchema);
+
+module.exports = Tutorial;
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const Category = require('./Category');
+const Tutorial = require('./Tutorial');
 
 module.exports = {
-    Category
+    Category,
+    Tutorial
 }
