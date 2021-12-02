051ccd345c3c1aabe7652da8c1223744444aa923
George Sandoval <gssandoval09@gmail.com>
2021-12-01 23:04:10 -0600

    added Comment.js file

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,11 @@
 const Category = require('./Category');
 const Tutorial = require('./Tutorial');
 const Rating = require('./Rating');
+const Comment = require('./Comment');
 
 module.exports = {
     Category,
     Tutorial,
-    Rating
+    Rating,
+    Comment
 }
