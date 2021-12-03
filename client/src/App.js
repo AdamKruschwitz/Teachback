18f204b76d83de7b1f4162b9c59cc86376ea284b
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-12-03 11:46:10 -0800

    implemented react-router-dom in app.js and index.js:

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
 import { Header } from './components'
 
 
@@ -22,7 +23,18 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-    <Header />
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" />
+        <Route path="/browse" />
+        <Route path="/profile" />
+        <Route path="/register" />
+        <Route path="/room/:id" />
+        <Route path="/tutorial/create" />
+        <Route path="*" />
+      </Routes>
+    </div>
   );
 }
 
