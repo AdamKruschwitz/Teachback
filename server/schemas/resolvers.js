05f319029f78f8ceb8e04eeb75dcae3dae44e421
George Sandoval <gssandoval09@gmail.com>
2021-12-04 17:36:35 -0600

    removed depreciated functions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,51 +1,94 @@
 const { UserInputError, AuthenticationError } = require("apollo-server-errors")
-const resolvers = {
-    Category,
-    Tutorial,
-    Rating,
-    Comment,
-    User,
-    Tag,
-    Step,
-    Room,
-} require ("../models")
+const { User, Category, Tutorial, Room, Tag, Step, Rating }
+
 const resolvers = {
     Query: {
-        currentUser: async () => {
-            return null;
+        user: async (_, { username }) => {
+            return User.findOne({ username: username }).populate('tutorials')
         },
-        user: async(_, { username }) => {
-        return User.findOne({ username: username }).populate('tutorials')
+        category: async (_, { username }) => {
+            return Category.findOne({ categoryId });
+        },
+        tutorialSearch: async (_, { title }) => {
+            const args = title ? { title } : {};
+            return Tutorial.find(args)
+        },
+        tutorial: async (_, { id }) => {
+            return Tutorial.findOne({ _id: tagId })
+        },
+        room: async (_, { id }) => {
+            return Tag.findOne({ _id: tagId })
+        },
+        Mutation: {
+            addUser: async (_, { args }) => {
+                try {
+                    let res = await User.create({ args });
+                    return res;
+                }
+                catch (e) {
+                    return e.message;
+                }
+            },
+            createTutorial: async (_, { TutorialInput }) => {
+                const createTutorial = await User.createTutorial({ _id, });
+            },
+            addComment: async (_, { stepId, content }) => {
+                if (user) {
+                    return Comment.findOneAndUpdate(
+                        { _id: ID, author },
+                        {
+                            $addToSet: {
+                                comments: { _id, author, content },
+                            },
+                        },
+                        {
+                            new: true,
+                            runValidators: true,
+                        }
+                    );
+                }
+                throw new AuthenticationError('Must be logged in first');
+            },
+            deleteComment: async (_, { commentId }, context) => {
+                if (context.user) {
+                    return Comment.findOneAndDelete(
+                        { _id: commentId },
+                        {
+                            $pull: {
+                                comments: {
+                                    _id: commentId,
+                                    commentAuthor: context.user.username,
+                                },
+                            },
+                        },
+                        { new: true }
+                    );
+                }
+                throw new AuthenticationError('You need to be logged in first');
+            },
+            addRating: async (_, { ratingId }, context) => {
+                if (context.user) {
+                    return Rating.findOne({ _id });
+                }
+            },
+            deleteRating: async (_, { ratingId }, context) => {
+                if (context.user) {
+                    return Rating.findOneAndUpdate(
+                        { _id: userId },
+                        {
+                            $pull: {
+                                ratings: {
+                                    _id: ratingId,
+                                    ratingId: context.user,
+                                },
+                            },
+                        },
+                        { new: true }
+                    );
+                }
+                throw new AuthenticationError('You need to be logged in first')
+            },
+        },
     },
-    category: async (_, { username }) => {
-        return Category.findOne({ categoryId });
-    },
-    tutorialSearch: async (_, { title }) => {
-        const args = title ? { title } : {};
-        return Tutorial.find(args)
-    },
-    tutorial: async (_, { id}) => {
-        return Tutorial.findOne({ _id: tagId })
-    },
-    room: async (_, { id }) => {
-        return Tag.findOne({ _id: tagId })
-    },
-    Mutation: {
-        addUser: async  (_, {args }) => {
-            try{
-                let res = await User.create({ args });
-                return res;
-            }
-        }
-    }
-    },
-
-
-
 }
-}
-
-    
-}
-
-module.exports = resolvers
+module.exports = resolvers;
