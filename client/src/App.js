4aef6057b9ad0254cdb0590534cca58ed67cdcc3
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-03 21:56:58 -0800

    finish styling and positioning of profile page, functionality left

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Header, Footer } from './components';
-import { Home, CreateTutorial, Browse, Profile, Room, Category } from './pages';
+import { 
+  Home, 
+  CreateTutorial, 
+  Browse, 
+  Profile, 
+  Room
+} from './pages';
 
 function App() {
   return (
     <div>
       <Header />
-      <Category />
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/browse" element={ <Browse /> } />
