8e0a9a18fff247a3e08e2ddc82de024c736f3c35
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-04 15:31:01 -0800

    finish browsing page design

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -1,7 +1,106 @@
 import React from 'react';
+import { Category } from '../components';
+import { SearchBar } from '../components';
+import styled from 'styled-components'
+import { Button } from '@mui/material';
 
 const Browse = function() {
-    // TODO
+    
+    return (
+        <MainBody>
+            <TopContainer>
+                <LeftContainer>
+                    <h1>Technologies</h1>
+                    <Button>Javascript</Button>
+                    <Button>HTML</Button>
+                    <Button>CSS</Button>
+                    <Button>React</Button>
+                    <Button>Reavt Native</Button>
+                    <Button>IOS</Button>
+                </LeftContainer>
+                <RightContainer>
+                    <h1>Popular Tags</h1>
+                    <Button>Swift</Button>
+                    <Button>GoLang</Button>
+                    <Button>Flutter</Button>
+                    <Button>Javascript</Button>
+                    <Button>Jquery</Button>
+                    <Button>Next.js</Button>
+                </RightContainer>
+            </TopContainer>
+            <SearchContainer>
+                <SearchBar />
+            </SearchContainer>
+            <CategoryContainer>
+                <h1>Categories</h1>
+                <Category />
+                <Category />
+                <Category />
+                <Category />
+                <Category />
+                <Category />
+            </CategoryContainer>
+        </MainBody>
+    )
 }
 
 export default Browse
+
+const MainBody = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #F0F0F0;
+`
+const TopContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    /* flex: 0.5; */
+    height: 450px;
+    width: 100vw;
+    background-color: var(--dark-purple);
+`
+const CategoryContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 0.5;
+
+`
+const LeftContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.5;
+    font-family: Montserrat;
+
+    > h1 {
+        color: #fff;
+    }
+    > button {
+        color: #fff;
+    }
+`
+const RightContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.5;
+    font-family: Montserrat;
+    
+    > h1 {
+        color: #fff;
+
+    }
+
+    > button {
+        color: #fff;
+    }
+`
+const SearchContainer = styled.div`
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
