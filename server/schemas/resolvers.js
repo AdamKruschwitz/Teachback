04a40792ceef911a7854cc1e49e9a4f67713e3e9
George Sandoval <gssandoval09@gmail.com>
2021-12-03 23:57:40 -0600

    Started working on resolversjs file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,50 @@
+const { UserInputError, AuthenticationError } = require("apollo-server-errors")
 const resolvers = {
-    // Query: {me: () => {
-    //     return 'helloworld';
-    // }},
+    Category,
+    Tutorial,
+    Rating,
+    Comment,
+    User,
+    Tag,
+    Step,
+    Room,
+} require ("../models")
+const resolvers = {
+    Query: {
+        currentUser: async () => {
+            return null;
+        },
+        user: async(_, { username }) => {
+        return User.findOne({ username: username }).populate('tutorials')
+    },
+    category: async (_, { username }) => {
+        return Category.findOne({ categoryId });
+    },
+    tutorialSearch: async (_, { title }) => {
+        const args = title ? { title } : {};
+        return Tutorial.find(args)
+    },
+    tutorial: async (_, { id}) => {
+        return Tutorial.findOne({ _id: tagId })
+    },
+    room: async (_, { id }) => {
+        return Tag.findOne({ _id: tagId })
+    },
+    Mutation: {
+        addUser: async  (_, {args }) => {
+            try{
+                let res = await User.create({ args });
+                return res;
+            }
+        }
+    }
+    },
+
+
+
+}
+}
+
     
 }
 
