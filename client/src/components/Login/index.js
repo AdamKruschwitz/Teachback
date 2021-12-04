cd7aa2865143f2a58ec77241639f977aafa98376
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-12-03 17:10:13 -0800

    added Login skeleton and updated index.js

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Dialog from '@mui/material/Dialog';
+import TextField from '@mui/material/TextField';
+import Tabs from '@mui/material/Tabs'
+
+const LoginDialogue = function() {
+
+}
+
+export default LoginDialogue;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,11 @@
 import Header from './Header'
+import Footer from './Footer'
+import SearchBar from './SearchBar/Search'
+import Login from './Login'
 
 export {
     Header,
+    Footer,
+    SearchBar,
+    Login
 }
