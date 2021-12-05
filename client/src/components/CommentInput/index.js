65fe125a7de02237596fe94bc6fa931bad069415
George Sandoval <gssandoval09@gmail.com>
2021-12-04 20:59:36 -0600

    added changes, added value and onChange

--- client/src/components/CommentInput/index.js
+++ client/src/components/CommentInput/index.js
@@ -1,64 +1,51 @@
 import React from "react";
-import styled from "styled-components";
 import TextField from '@mui/material/TextField'
-// import Box from '@mui/material/Box'
-// import Input from "@mui/material/Input";
-// import InputAdornment from "@mui/material/InputAdornment";
+import Box from '@mui/material/Box'
+import Input from "@mui/material/Input";
+import styled from "styled-components";
+import { Button } from '@mui/material';
+
 
 //TO-DO: Get container for comment section here
-export deafult function FormPropsTextFields() {
+export default function CommentInput() {
+    const [value, setValue] = React.useState('Controlled');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+
     return (
-        <Box 
-        component="form"
-        sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch'},
-        }}
-        noValidate
-        autoComplete="off"
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
         >
-        <div>
-            <Textfield 
-                required
-                id="filled-required"
-                label="Required"
-                defaultValue="This is a test"
-                variant="filled"
+            <div>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Multiline"
+                    multiline
+                    rows={4}
+                    defaultValue="Default Value"
+                    onChange={handleChange}
+                    value={value}
                 />
-            <TextField
-            disabled
-            id="filled-disabled"
-            label="Disabled"
-            defaultValue="This is a test"
-            variant="filled"
-            />
-            <TextField
-            id="filled-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="filled"
-            />
+            </div>
             
+            <div>
+            <Button 
+            onClick={() => {
+               //TODO: implement comment submission
+                alert('clicked'), variant="contained";
+            }}
+            >
+                Submit </Button>
+            </div>
+        </Box>
+    );
+}
 
-        </div>
-
-// const InputComponent = ({ placeholder, type }) => {
-//     return (
-//         <div>
-//             <InputContainer
-//                 placeholder={placeholder}
-//                 startAdornment={
-//                     type === "Search" ? (
-//                         <InputAdornment position="start">
-
-//                         </InputAdornment>
-//                     ) : (
-//                         ""
-//                     )
-//                 }
-//                 />
-//         </div>
-//     );
-// };
-
-export default InputComponent;
