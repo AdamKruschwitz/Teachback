2ba503f1754b439504186f3eb0401daa324c124a
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-12-04 17:53:10 -0800

    Finished middle text input container, added steps buttons

--- client/src/components/TutorialInput/index.js
+++ client/src/components/TutorialInput/index.js
@@ -51,77 +51,97 @@ function TutorialInput({ className }) {
 
   return (
     <MainContainer>
+
       <TopContainer>
+
         <TitleContainer>
           <Input sx={{ width: 300, backgroundColor: 'white' }} placeholder="Title..." />
         </TitleContainer>
+
         <CategoryContainer>
         <Box sx={{ width: 300 }}>
           <FormControl sx={{ width: 300, backgroundColor: "white", color: "#94ECBE" }}>
             <InputLabel sx={{ color: "#94ECBE" }} id="demo-simple-select-label">Category</InputLabel>
-            <Select
-              fullWidth
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={category}
-              label="Category"
-              onChange={handleCategory}
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
+                <Select
+                fullWidth
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={category}
+                label="Category"
+                onChange={handleCategory}
+                >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
           </Box>
-
         </CategoryContainer>
+
         <TagContainer>
           <Input sx={{ width: 300, backgroundColor: 'white' }} placeholder="Tags..." />
         </TagContainer>
       </TopContainer>
+
+      <MiddleTitleContainer>
+            <MiddleLeftContainer>
+                <h1>Write Here</h1>
+            </MiddleLeftContainer>
+
+            <MiddleRightContainer>
+                <h1>Preview</h1>
+            </MiddleRightContainer>
+      </MiddleTitleContainer>
+
       <MiddleContainer>
           <LeftContainer>
           <TextField
+          sx= {{fontSize: "20px"}}
           multiline
           name="Input"
           value={body}
-          rows={15}
+          rows={30}
           onChange={handleChange}
           className={className}
         />
           </LeftContainer>
-          <hr />
           <RightContainer>
         <TextField
+            sx= {{fontSize: "20px"}}
           multiline
           name="Input"
           value={body}
-          rows={15}
+          rows={30}
           onChange={handleChange}
           className={className}
         />
         </RightContainer>
-       <MiddleTitleContainer>
-           <MiddleLeftContainer>
-                <h1>Write Here</h1>
-           </MiddleLeftContainer>
-           <MiddleRightContainer>
-           <h1>Preview</h1>
-           </MiddleRightContainer>
-       </MiddleTitleContainer>
-      </MiddleContainer>
-      <Button
-          variant="contained"
-          onClick={togglePreview}
-          children="Preview"
-        />
-        <Button variant="contained" onClick={toggleSubmit} children="Submit" />
+        </MiddleContainer>
+
+        <ButtonContainer>
+            <StepsContainer>
+               <Button>1</Button>
+               <Button>1</Button>
+               <Button>1</Button>
+               <Button>+</Button>
+            </StepsContainer>
+            <ButtonRight>
+                <Button
+                variant="contained"
+                onClick={togglePreview}
+                children="Preview"
+                />
+                <Button sx={{backgroundColor: "#94ECBE !important", color: "#2D2244 !important", border: "1px solid #2D2244 !important"}} variant="contained" onClick={toggleSubmit} children="Submit" 
+                />
+            </ButtonRight>
+        </ButtonContainer>
+      
     </MainContainer>
   );
 }
 
 const StyledTutorialInput = styled(TutorialInput)`
-  width: 100vw;
+  /* width: 100vw; */
   border-radius: 4px #2d2244;
   margin-left: 20px;
 `;
@@ -171,11 +191,23 @@ const TagContainer = styled.div`
 `;
 
 const MiddleTitleContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    font-family: Montserrat;
+
 `
 
 const MiddleLeftContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex: 0.5;
 `
 const MiddleRightContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex: 0.5;
 `
 
 const MiddleContainer = styled.div`
@@ -188,26 +220,68 @@ const MiddleContainer = styled.div`
 `;
 
 const LeftContainer = styled.div`
-    /* flex-direction: column; */
+    flex-direction: column;
     display: flex;
     flex: 0.5;
+    margin: 10px;
 `
 
 const RightContainer = styled.div`
-    /* flex-direction: column; */
+    flex-direction: column;
     display: flex;
     flex: 0.5;
+    margin: 10px;
 
 `
 
+const ButtonContainer = styled.div`
+display: flex;
+justify-content: flex-end;
 
-// > button {
-//     border: 3px solid #94ecbe !important;
-//     margin-bottom: 15px !important;
-//     background-color: #fff !important;
-//     color: var(--light-green) !important;
-//     border-color: var(--light-green) !important;
-//   }
+
+`
+
+const StepsContainer = styled.div`
+    display: flex;
+    flex: 0.5;
+    margin-left: 10px;
+
+    > button {
+        width: 25px;
+        height: 60px;
+        border-radius: 50%;
+        background-color: #fff;
+        color: #111;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        margin: 10px;
+
+        :hover {
+            background-color: var(--light-green);
+            color: var(--dark-purple);
+        }
+    }
+`
+const ButtonRight = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    flex: 0.5;
+    margin: 10px;
+
+    > button {
+    border: 1px solid #94ecbe;
+    margin-bottom: 15px !important;
+    background-color: #fff;
+    color: var(--light-green);
+    border-color: var(--light-green);
+    margin: 10px;
+    :hover {
+        border: none;
+        background-color: #2D2244;
+
+    }
+
+  }
+`
 
 
 
