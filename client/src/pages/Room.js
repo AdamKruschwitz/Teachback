5e8a590b4e17f3276df5b04f51ad25a06c7eb6e7
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-04 17:16:46 -0800

    add first stricture

---
+++ client/src/pages/Room.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button } from '@mui/material';
+
+function Room() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Room
