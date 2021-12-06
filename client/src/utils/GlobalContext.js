b0dba5ea83458619c59e07f9db527a51b7d5e5de
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-06 10:47:34 -0800

    user persistent login done

--- client/src/utils/GlobalContext.js
+++ client/src/utils/GlobalContext.js
@@ -1,5 +1,7 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 import { useGlobalReducer } from './reducers';
+import { auth } from '../firebase'
+import { PASSWORD_LOGIN } from './actions';
 
 const GlobalContext = createContext();
 const { Provider } = GlobalContext;
@@ -11,7 +13,21 @@ const initialState = {
 
 export function GlobalProvider({ value = [], ...props }) {
     const [state, dispatch] = useGlobalReducer(initialState);
-  
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(result => {
+            console.log(result)
+            const token = result.refreshToken;
+              const user = {
+                  username: result.email,
+                  email: result.email,
+                  image: '',
+                  toke: token
+              }
+              dispatch({type: PASSWORD_LOGIN, payload: user });
+        })
+    
+        return unsubscribe
+      }, [])
     return <Provider value={[state, dispatch]} {...props} />;
   };
 
