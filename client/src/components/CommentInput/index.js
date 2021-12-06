9b4ab7572d095f4537ee363ae9fa46d96b1a0aef
George Sandoval <gssandoval09@gmail.com>
2021-12-05 18:11:25 -0600

    updated comment input

--- client/src/components/CommentInput/index.js
+++ client/src/components/CommentInput/index.js
@@ -1,53 +1,31 @@
 import React from "react";
 import TextField from '@mui/material/TextField'
-import Box from '@mui/material/Box'
 import Input from "@mui/material/Input";
 import styled from "styled-components";
-import { Button } from '@mui/material';
-
+import Button  from '@mui/material/Button';
 
 //TO-DO: Get container for comment section here
 export default function CommentInput() {
-    const [value, setValue] = React.useState('Controlled');
+    const [value, setValue] = React.useState('');
 
     const handleChange = (event) => {
         setValue(event.target.value);
     };
-
-
     return (
-        <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <div>
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Multiline"
-                    multiline
-                    rows={4}
-                    defaultValue="Default Value"
-                    onChange={handleChange}
-                    value={value}
-                />
-            </div>
-            
-            <div>
-            <Button 
-            onClick={() => {
-               //TODO: implement comment submission
-               
-                // alert('clicked'), variant="contained";
-            }}
-            >
-                Submit </Button>
-            </div>
-        </Box>
-    );
-}
+        <div>
+            <TextField
+                id="textfield"
+                label="Enter Comment here"
+                multiline
+                defaultValue="Default Value"
+                onChange={handleChange}
+                value={value}
+                TextField style={{ width: '25%' }}
+            />
+            <Button onClick={() => {
 
+            }}> Submit</Button>
+        </div>
+    )
+}
 
--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -4,7 +4,7 @@ const Room = function() {
     // TODO
     return (
         <CommentInput />
-
+        
     )
 }
 
